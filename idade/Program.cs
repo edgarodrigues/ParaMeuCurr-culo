91d641503f9d4cd5d973232f331cb64f218587e7
Edgar Rodrigues <edgarodgui@gmail.com>
2024-10-02 10:26:00 -0300

    Adicionando arquivo

--- idade/Program.cs
+++ idade/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Qual é a idade? ");
+int idade =  int.Parse(Console.ReadLine());
+
+Console.WriteLine($"A idade é: {idade}");
