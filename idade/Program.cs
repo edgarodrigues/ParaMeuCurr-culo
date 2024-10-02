913a9fc5421f74523a901cd58fdc4067967d954a
Edgar Rodrigues <edgarodgui@gmail.com>
2024-10-02 11:19:06 -0300

    Adicionando arquivo

--- idade/Program.cs
+++ idade/Program.cs
@@ -1,5 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Qual é a idade? ");
-int idade =  int.Parse(Console.ReadLine());
+int idade = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"A idade é: {idade}");
+
+{
+
+    if (idade >= 0 && idade <= 12)
+    {
+        Console.WriteLine("criança");
+    }
+    else if (idade >= 13 && idade <= 17)
+    {
+        Console.WriteLine("adolescente");
+    }
+    else if (idade >= 18 && idade <= 64)
+    {
+        Console.WriteLine("adulto");
+    }
+    else if (idade >= 65)
+    {
+        Console.WriteLine("idoso");
+    }
+
+
+
+    else
+    {
+        Console.WriteLine("erro: nenhum número informado");
+
+
+    }
+
+
